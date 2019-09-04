bc515965ac1b00af0200de39e3800cb354f27c42
Peter Li <peter.li.work@gmail.com>
2019-09-04 12:25:57 -0400

    adding bootstrap

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -2,7 +2,16 @@ import React, { component } from "react";
 import Aux from "../Aux/Aux";
 
 class Layout extends Component {
+  state = {
+    isAuthenticated: false
+  };
   render() {
-    return <Aux>{this.props.children}</Aux>;
+    return (
+      <Aux>
+        <NavigationItems />
+        {this.props.children}
+      </Aux>
+    );
   }
 }
+export default Layout;
